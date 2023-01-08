2b6b110e60f05bf34a7b34eb7889ff2109151e3e
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-01-08 00:52:34 +0000

    [nvim] rewrite

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -1,4 +1,4 @@
-require'bufferline'.setup{
+require('bufferline').setup {
   icons = false,
   animation = false,
 
@@ -27,7 +27,7 @@ end)
 
 -- keybindings
 local map = vim.keymap.set
-map({'n', 'i'}, '<A-l>', ':BufferNext<CR>')
-map({'n', 'i'}, '<A-h>', ':BufferPrevious<CR>')
+map({'n', 'v', 'i'}, '<A-l>', ':BufferNext<CR>')
+map({'n', 'v', 'i'}, '<A-h>', ':BufferPrevious<CR>')
 map('n', '<A-c>', ':BufferClose<CR>')
 
