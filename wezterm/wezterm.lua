da882f209e2efcd4c64be61888aaba9736b18444
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-04-13 20:23:21 +0100

    Wez's Terminal Emulator

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,37 @@
+local wezterm = require('wezterm')
+
+local config = {}
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font = wezterm.font('JetBrains Mono', { weight = 'Regular' })
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0
+}
+
+config.skip_close_confirmation_for_processes_named = {
+    'bash',
+    'sh',
+    'zsh',
+    'fish',
+    'tmux',
+    'nu',
+    'cmd.exe',
+    'pwsh.exe',
+    'powershell.exe',
+}
+
+config.keys = {
+  { key = 'n', mods = 'ALT', action = wezterm.action.ShowLauncher },
+}
+
+config.enable_scroll_bar = true
+config.font_size = 9.5
+config.default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe", '-l' }
+config.use_fancy_tab_bar = false
+
+return config
