04d580d9301625d292ca524c578824425f4e315d
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-04-16 20:16:55 +0100

    [pwsh] XDGBDS $env variables

--- Powershell/profile.ps1
+++ Powershell/profile.ps1
@@ -1,3 +1,8 @@
+$env:XDG_CONFIG_HOME = "$env:userprofile\.config\"
+$env:XDG_CACHE_HOME="$env:userprofile\.cache\"
+$env:XDG_DATA_HOME="$env:userprofile\.local\share\"
+$env:XDG_STATE_HOME="$env:userprofile\.local\state\"
+
 function Prompt {
 	$promptString = "$(Get-Location)>"
 	Write-Host $promptString -NoNewline -ForegroundColor Magenta
