2b65586c727e0f2154667067f35febacb825bb92
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-06-11 14:56:57 +0100

    [pwsh] user folder is a mess
    https://twitter.com/endermanch/status/1667095426212700160

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,7 +1,7 @@
-$env:XDG_CONFIG_HOME = "$env:userprofile\.config\"
-$env:XDG_CACHE_HOME="$env:userprofile\.cache\"
-$env:XDG_DATA_HOME="$env:userprofile\.local\share\"
-$env:XDG_STATE_HOME="$env:userprofile\.local\state\"
+# $env:XDG_CONFIG_HOME = "$env:userprofile\.config\"
+# $env:XDG_CACHE_HOME="$env:userprofile\.cache\"
+# $env:XDG_DATA_HOME="$env:userprofile\.local\share\"
+# $env:XDG_STATE_HOME="$env:userprofile\.local\state\"
 
 function Prompt {
 	$promptString = "$(Get-Location)>"
--- powershell/startup.ps1
+++ powershell/startup.ps1
@@ -1,4 +1,4 @@
-Set-Content -Path "C:\Program Files\PowerShell\7\profile.ps1" -Value ". $env:userprofile\.config\powershell\profile.ps1"
+# Set-Content -Path "C:\Program Files\PowerShell\7\profile.ps1" -Value ". $env:userprofile\.config\powershell\profile.ps1"
 
-&"C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe" "$env:XDG_CONFIG_HOME\autohotkey\config.ahk"
+# &"C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe" "$env:XDG_CONFIG_HOME\autohotkey\config.ahk"
 
