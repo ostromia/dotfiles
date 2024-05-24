b337ea12fe7ed62ea15c8e763cbe35c40e9a12b2
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2024-05-24 13:05:48 +0100

    create readme generator

---
+++ .docs/generate_readme.py
@@ -0,0 +1,58 @@
+import os
+import re
+
+def set_section_title_and_description(file):
+    file.write(
+        """# dotfiles\n\ntrying to figure out how VimScript works\n\n"""
+    )
+
+def set_section_software(file):
+    file.write(
+"""## Software
+
+| Software                                             | Source Code                                |
+|------------------------------------------------------|--------------------------------------------|
+| [a-Shell](https://holzschu.github.io/a-Shell_iOS/)   | https://github.com/holzschu/a-shell        |
+| [AutoHotKey](https://autohotkey.com/)                | https://github.com/AutoHotkey/AutoHotkey   |
+| [Bash](https://www.gnu.org/software/bash/)           | https://git.savannah.gnu.org/cgit/bash.git |
+| [Git](https://git-scm.com/)                          | https://git.kernel.org/pub/scm/git/git.git |
+| [Neovim](https://neovim.io/)                         | https://github.com/neovim/neovim           |
+| [PowerShell](https://microsoft.com/PowerShell)       | https://github.com/PowerShell/PowerShell   |
+| [tmux](https://tmux.github.io/)                      | https://github.com/tmux/tmux               |
+| [Vifm](https://vifm.info/)                           | https://github.com/vifm/vifm               |
+| [Visual Studio Code](https://code.visualstudio.com/) | https://github.com/microsoft/vscode        |
+| [WezTerm](https://wezfurlong.org/wezterm/index.html) | https://github.com/wez/wezterm             |
+
+"""
+    )
+
+def set_section_neovim_plugins(markdownFile, configFile):
+    markdownFile.write("## Neovim Plugins\n\n")
+
+    pattern = r"call plug#begin\(\)(.*?)call plug#end\(\)"
+    vimplug = re.search(pattern, configFile.read(), re.DOTALL)
+    vimplug = vimplug.group(1).strip()
+
+    for i in vimplug.split("\n"):
+        pattern = r"Plug '(.*?)'"
+        match = re.match(pattern, i)
+
+        if match:
+            pluginName = match.group(1)
+            pluginURL = "https://github.com/" + pluginName
+            markdownFile.write(f"- [{pluginName}]({pluginURL})\n")
+
+    markdownFile.write("\n")
+
+if __name__ == "__main__":
+    cwd = os.path.dirname(__file__)
+    readmeMD = open( cwd + r"\..\README.md", "w+" )
+    initVIM = open(  cwd + r"\..\nvim\init.vim"   )
+
+    set_section_title_and_description(readmeMD)
+    set_section_software(readmeMD)
+    set_section_neovim_plugins(readmeMD, initVIM)
+
+    readmeMD.close()
+    initVIM.close()
+
---
+++ README.md
@@ -0,0 +1,35 @@
+# dotfiles
+
+trying to figure out how VimScript works
+
+## Software
+
+| Software                                             | Source Code                                |
+|------------------------------------------------------|--------------------------------------------|
+| [a-Shell](https://holzschu.github.io/a-Shell_iOS/)   | https://github.com/holzschu/a-shell        |
+| [AutoHotKey](https://autohotkey.com/)                | https://github.com/AutoHotkey/AutoHotkey   |
+| [Bash](https://www.gnu.org/software/bash/)           | https://git.savannah.gnu.org/cgit/bash.git |
+| [Git](https://git-scm.com/)                          | https://git.kernel.org/pub/scm/git/git.git |
+| [Neovim](https://neovim.io/)                         | https://github.com/neovim/neovim           |
+| [PowerShell](https://microsoft.com/PowerShell)       | https://github.com/PowerShell/PowerShell   |
+| [tmux](https://tmux.github.io/)                      | https://github.com/tmux/tmux               |
+| [Vifm](https://vifm.info/)                           | https://github.com/vifm/vifm               |
+| [Visual Studio Code](https://code.visualstudio.com/) | https://github.com/microsoft/vscode        |
+| [WezTerm](https://wezfurlong.org/wezterm/index.html) | https://github.com/wez/wezterm             |
+
+## Neovim Plugins
+
+- [navarasu/onedark.nvim](https://github.com/navarasu/onedark.nvim)
+- [nvim-tree/nvim-web-devicons](https://github.com/nvim-tree/nvim-web-devicons)
+- [nvim-tree/nvim-tree.lua](https://github.com/nvim-tree/nvim-tree.lua)
+- [romgrk/barbar.nvim](https://github.com/romgrk/barbar.nvim)
+- [nvim-lualine/lualine.nvim](https://github.com/nvim-lualine/lualine.nvim)
+- [junegunn/fzf](https://github.com/junegunn/fzf)
+- [numToStr/Comment.nvim](https://github.com/numToStr/Comment.nvim)
+- [norcalli/nvim-colorizer.lua](https://github.com/norcalli/nvim-colorizer.lua)
+- [nvim-treesitter/nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter)
+- [nvim-treesitter/nvim-treesitter-textobjects](https://github.com/nvim-treesitter/nvim-treesitter-textobjects)
+- [m-demare/hlargs.nvim](https://github.com/m-demare/hlargs.nvim)
+- [neovim/nvim-lspconfig](https://github.com/neovim/nvim-lspconfig)
+- [mfussenegger/nvim-lint](https://github.com/mfussenegger/nvim-lint)
+
