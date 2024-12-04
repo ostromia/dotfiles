697d42cb19ecc9c4cf56a14cb48518249f728670
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2024-12-04 22:48:37 +0000

    add script to move game config

---
+++ steam.ps1
@@ -0,0 +1,3 @@
+Copy-Item -Path "$HOME\GitHub\dotfiles\csgo\autoexec.cfg" -Destination "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\autoexec.cfg" -Force
+Copy-Item -Path "$HOME\GitHub\dotfiles\tf2\autoexec.cfg" -Destination "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf\cfg\autoexec.cfg" -Force
+
