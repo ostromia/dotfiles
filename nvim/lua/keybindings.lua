93768c4b8ddf3df61f55d03e11c3dc6f5c64cda8
ostromia <66202981+ostromia@users.noreply.github.com>
2025-05-22 13:51:32 +0100

    [nvim] set y & p to named register

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -8,6 +8,10 @@ vim.keymap.set('n', '<D-o>', ":FZF<CR>")
 
 vim.keymap.set('n', 'w', 'b')
 
--- copy & paste to system clipboard
+-- copy & paste to named register & system clipboard
 vim.keymap.set({'n', 'v'}, '<S-y>', '"+y')
 vim.keymap.set({'n', 'v'}, '<S-p>', '"+p')
+
+vim.keymap.set({"n", "v"}, "y", '"ay', { noremap = true })
+vim.keymap.set({"n", "v"}, "p", '"ap', { noremap = true })
+
