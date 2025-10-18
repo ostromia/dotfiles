5d2b685b687fed8082b3cb8fb6af9876f1933368
ostromia <66202981+ostromia@users.noreply.github.com>
2025-10-18 17:23:41 +0100

    [pwsh] clean profile.ps1

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,21 +1,5 @@
-# $env:XDG_CONFIG_HOME = "$env:userprofile\.config\"
-# $env:XDG_CACHE_HOME="$env:userprofile\.cache\"
-# $env:XDG_DATA_HOME="$env:userprofile\.local\share\"
-# $env:XDG_STATE_HOME="$env:userprofile\.local\state\"
-
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-
 function Prompt {
     $promptString = "$(Get-Location)>"
     Write-Host $promptString -NoNewline -ForegroundColor Magenta
     return " "
 }
-
-$env:RUSTUP_HOME = "$env:userprofile\AppData\RUST\.rustup"
-$env:CARGO_HOME = "$env:userprofile\AppData\RUST\.cargo"
-
-New-Alias "vifm" "$env:userprofile\.binary\vifm-w64-se-0.13-binary\vifm.exe"
-New-Alias "fzf" "$env:LOCALAPPDATA\Microsoft\WinGet\Packages\junegunn.fzf_Microsoft.Winget.Source_8wekyb3d8bbwe\fzf.exe"
-New-Alias "python3" "python.exe"
-New-Alias "pyenva" ".\.venv\Scripts\activate.ps1"
-
