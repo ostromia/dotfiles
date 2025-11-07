caea9e724ae3ba88dbaf7d0899b1bff522986f8b
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2025-11-07 09:12:57 +0000

    [nvim] bind d to system clipboard

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -8,7 +8,7 @@ vim.keymap.set('n', '<D-o>', ":FZF<CR>")
 
 vim.keymap.set('n', 'w', 'b')
 
--- copy & paste
+vim.keymap.set({"n", "v"}, "d", '"+d', { noremap = true })
 vim.keymap.set({"n", "v"}, "y", '"+y', { noremap = true })
 vim.keymap.set({"n", "v"}, "p", '"+p', { noremap = true })
 
