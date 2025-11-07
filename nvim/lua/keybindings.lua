f61f569169d978e71aa1e05b944140b09f63552e
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2025-11-07 07:50:10 +0000

    [nvim] bind y & p to system clipboard

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -8,10 +8,7 @@ vim.keymap.set('n', '<D-o>', ":FZF<CR>")
 
 vim.keymap.set('n', 'w', 'b')
 
--- copy & paste to named register & system clipboard
-vim.keymap.set({'n', 'v'}, '<S-y>', '"+y')
-vim.keymap.set({'n', 'v'}, '<S-p>', '"+p')
-
-vim.keymap.set({"n", "v"}, "y", '"ay', { noremap = true })
-vim.keymap.set({"n", "v"}, "p", '"ap', { noremap = true })
+-- copy & paste
+vim.keymap.set({"n", "v"}, "y", '"+y', { noremap = true })
+vim.keymap.set({"n", "v"}, "p", '"+p', { noremap = true })
 
