65f2776ed39a6ff64fb492fafa2c87d478849228
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2025-11-27 12:57:39 +0000

    error handling for file operations

--- macos.py
+++ macos.py
@@ -3,14 +3,21 @@ from pathlib import Path
 import shutil
 
 def copy(src: Path, dst: Path) -> None:
-	source = Path(src).expanduser()
-	target = Path(dst).expanduser()
+	if not src.exists():
+		raise FileNotFoundError(f"No such file or directory: {src}")
+
+	if not dst.exists():
+		dst.parent.mkdir(parents=True, exist_ok=True)
+
+	if src.is_file() != dst.is_file() or src.is_dir() != dst.is_dir():
+		raise ValueError("Both file arguments must be the same type")
 
 	if src.is_file():
-		target.parent.mkdir(parents=True, exist_ok=True)
-		shutil.copy2(source, target)
+		shutil.copy2(src, dst)
 	else:
-		shutil.copytree(source, target, dirs_exist_ok=True)
+		shutil.copytree(src, dst, dirs_exist_ok=True)
+
+	print(f"{src} -> {dst}")
 
 def install(software: list[dict[str, Path]]):
 	for i in software:
