c1b4ee744015644a7a9e7fa7f5e9581561d5704c
ostromia <66202981+ostromia@users.noreply.github.com>
2026-01-25 15:52:19 +0000

    reminder never use tabs for indentation

--- macos.py
+++ macos.py
@@ -3,93 +3,93 @@ from pathlib import Path
 import shutil
 
 def copy(src: Path, dst: Path) -> None:
-	if not src.exists():
-		raise FileNotFoundError(f"No such file or directory: {src}")
+    if not src.exists():
+        raise FileNotFoundError(f"No such file or directory: {src}")
 
-	if not dst.exists():
-		dst.parent.mkdir(parents=True, exist_ok=True)
+    if not dst.exists():
+        dst.parent.mkdir(parents=True, exist_ok=True)
 
-	if src.is_file() != dst.is_file() or src.is_dir() != dst.is_dir():
-		raise ValueError("Both file arguments must be the same type")
+    if src.is_file() != dst.is_file() or src.is_dir() != dst.is_dir():
+        raise ValueError("Both file arguments must be the same type")
 
-	if src.is_file():
-		shutil.copy2(src, dst)
-	else:
-		shutil.copytree(src, dst, dirs_exist_ok=True)
+    if src.is_file():
+        shutil.copy2(src, dst)
+    else:
+        shutil.copytree(src, dst, dirs_exist_ok=True)
 
-	print(f"{src} -> {dst}")
+    print(f"{src} -> {dst}")
 
 def install(software: list[dict[str, Path]]):
-	for i in software:
-		copy(i["source"], i["target"])
+    for i in software:
+        copy(i["source"], i["target"])
 
 def backup(software: list[dict[str, Path]]):
-	for i in software:
-		copy(i["target"], i["source"])
+    for i in software:
+        copy(i["target"], i["source"])
 
 if __name__ == "__main__":
-	software = [
-		{
-			"source": "~/GitHub/dotfiles/ghostty/config",
-			"target": "~/.config/ghostty/config"
-		},
-		{
-			"source": "~/GitHub/dotfiles/git/",
-			"target": "~/.config/git/"
-		},
-		{
-			"source": "~/GitHub/dotfiles/karabiner/karabiner.json",
-			"target": "~/.config/karabiner/karabiner.json"
-		},
-		{
-			"source": "~/GitHub/dotfiles/npm/",
-			"target": "~/.config/npm/"
-		},
-		{
-			"source": "~/GitHub/dotfiles/nvim/",
-			"target": "~/.config/nvim/"
-		},
-		{
-			"source": "~/GitHub/dotfiles/vscode/keybindings.json",
-			"target": "~/Library/Application Support/Code/User/keybindings.json"
-		},
-		{
-			"source": "~/GitHub/dotfiles/vscode/settings.json",
-			"target": "~/Library/Application Support/Code/User/settings.json"
-		},
-		{
-			"source": "~/GitHub/dotfiles/zsh/.zprofile",
-			"target": "~/.zprofile"
-		},
-		{
-			"source": "~/GitHub/dotfiles/zsh/.zshenv",
-			"target": "~/.zshenv"
-		},
-		{
-			"source": "~/GitHub/dotfiles/zsh/.zshrc",
-			"target": "~/.zshrc"
-		},
-		{
-			"source": "~/GitHub/dotfiles/zsh/lss.lua",
-			"target": "~/.config/zsh/lss.lua"
-		}
-	]
+    software = [
+        {
+            "source": "~/GitHub/dotfiles/ghostty/config",
+            "target": "~/.config/ghostty/config"
+        },
+        {
+            "source": "~/GitHub/dotfiles/git/",
+            "target": "~/.config/git/"
+        },
+        {
+            "source": "~/GitHub/dotfiles/karabiner/karabiner.json",
+            "target": "~/.config/karabiner/karabiner.json"
+        },
+        {
+            "source": "~/GitHub/dotfiles/npm/",
+            "target": "~/.config/npm/"
+        },
+        {
+            "source": "~/GitHub/dotfiles/nvim/",
+            "target": "~/.config/nvim/"
+        },
+        {
+            "source": "~/GitHub/dotfiles/vscode/keybindings.json",
+            "target": "~/Library/Application Support/Code/User/keybindings.json"
+        },
+        {
+            "source": "~/GitHub/dotfiles/vscode/settings.json",
+            "target": "~/Library/Application Support/Code/User/settings.json"
+        },
+        {
+            "source": "~/GitHub/dotfiles/zsh/.zprofile",
+            "target": "~/.zprofile"
+        },
+        {
+            "source": "~/GitHub/dotfiles/zsh/.zshenv",
+            "target": "~/.zshenv"
+        },
+        {
+            "source": "~/GitHub/dotfiles/zsh/.zshrc",
+            "target": "~/.zshrc"
+        },
+        {
+            "source": "~/GitHub/dotfiles/zsh/lss.lua",
+            "target": "~/.config/zsh/lss.lua"
+        }
+    ]
 
-	software2 = []
+    software2 = []
 
-	for i in software:
-		j = {
-			"source": Path(i["source"]).expanduser(),
-			"target": Path(i["target"]).expanduser()
-		}
-		software2.append(j)
+    for i in software:
+        j = {
+            "source": Path(i["source"]).expanduser(),
+            "target": Path(i["target"]).expanduser()
+        }
+        software2.append(j)
 
-	if len(sys.argv) != 2:
-		sys.exit(f"Usage: {sys.argv[0]} [install|backup]")
+    if len(sys.argv) != 2:
+        sys.exit(f"Usage: {sys.argv[0]} [install|backup]")
 
-	argument = sys.argv[1].lower()
+    argument = sys.argv[1].lower()
 
-	if argument == "install":
-		install(software2)
-	elif argument == "backup":
-		backup(software2)
+    if argument == "install":
+        install(software2)
+    elif argument == "backup":
+        backup(software2)
