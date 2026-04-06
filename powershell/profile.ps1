be6aa3239a3b4396ec3c186d5b6a44a064f982cb
ostromia <66202981+ostromia@users.noreply.github.com>
2026-04-06 17:34:58 +0100

    [pwsh] emit osc 7

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,5 +1,8 @@
 function Prompt {
-    $promptString = "$(Get-Location)>"
+    $cwd = (Get-Location).Path
+    $osc7 = "`e]7;file:///$($cwd -replace '\\', '/')`a"
+    Write-Host $osc7 -NoNewline
+    $promptString = "${cwd}>"
     Write-Host $promptString -NoNewline -ForegroundColor Magenta
     return " "
 }
