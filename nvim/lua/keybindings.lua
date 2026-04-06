54b663328116b81f110eb70abe3233c04e854174
ostromia <66202981+ostromia@users.noreply.github.com>
2026-04-06 18:23:23 +0100

    [nvim] set cmd keybinds on mac to alt for windows

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -1,8 +1,11 @@
+local M = {}
+M.mod = vim.fn.has("mac") == 1 and "D" or "M"
+
 vim.g.mapleader = ' '
 
 vim.keymap.set('n', ';', ':')
 
-vim.keymap.set({'n', 'v', 'i'}, '<D-a>', '<Esc>')
+vim.keymap.set({'n', 'v', 'i'}, '<' .. M.mod .. '-a>', '<Esc>')
 
 vim.keymap.set('n', 'w', 'b')
 
@@ -10,3 +13,4 @@ vim.keymap.set({"n", "v"}, "d", '"+d', { noremap = true })
 vim.keymap.set({"n", "v"}, "y", '"+y', { noremap = true })
 vim.keymap.set({"n", "v"}, "p", '"+p', { noremap = true })
 
+return M
--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -1,6 +1,8 @@
 local telescope = require("telescope")
 local builtin = require('telescope.builtin')
 
+local mod = require("keybindings").mod
+
 telescope.setup({
     extensions = {
         file_browser = {
@@ -10,7 +12,7 @@ telescope.setup({
     }
 })
 
-vim.keymap.set("n", "<D-o>", telescope.extensions.file_browser.file_browser)
+vim.keymap.set("n", "<" .. mod .. "-o>", telescope.extensions.file_browser.file_browser)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep)
 vim.keymap.set('n', '<leader>fb', builtin.buffers)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags)
