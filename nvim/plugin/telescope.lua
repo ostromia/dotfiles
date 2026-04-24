5b1fdc725df52f0ecb94373baed199aa1afc05db
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2026-04-24 17:46:41 +0100

    [nvim] telescope command palette

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -17,5 +17,36 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep)
 vim.keymap.set('n', '<leader>fb', builtin.buffers)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags)
 
+local lsp_commands = {
+}
+
+local function command_palette()
+    local pickers = require("telescope.pickers")
+    local finders = require("telescope.finders")
+    local conf = require("telescope.config").values
+    local actions = require("telescope.actions")
+    local action_state = require("telescope.actions.state")
+
+    pickers.new({}, {
+        finder = finders.new_table({
+            results = lsp_commands,
+            entry_maker = function(entry)
+                return { value = entry, display = entry.name, ordinal = entry.name }
+            end,
+        }),
+        sorter = conf.generic_sorter({}),
+        attach_mappings = function(buf, map)
+            actions.select_default:replace(function()
+                local action = action_state.get_selected_entry().value.action
+                actions.close(buf)
+                vim.schedule(action)
+            end)
+            return true
+        end,
+    }):find()
+end
+
+vim.keymap.set("n", "<" .. mod .. "-p>", command_palette)
+
 require("telescope").load_extension("file_browser")
 
