d968dfd4558f3a25ad27f2791c5d15f3889b0e8a
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2026-08-08 14:52:53 +0100

    [nvim] rewrite options.lua

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,39 +1,31 @@
--- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
--- disable python stuff
 vim.g.loaded_python3_provider = 0
-vim.g.python_recommended_style = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
 
--- set options
 vim.o.swapfile = false
 
-vim.wo.number = true
-vim.wo.relativenumber = true
+vim.o.termguicolors = true
 
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
-vim.o.list = false
-vim.o.listchars = "eol:$,tab:>-,space:·"
+vim.opt.list = false
+vim.opt.listchars = { eol = "$", tab = ">-", space = "·" }
 
-vim.wo.signcolumn = "yes"
-vim.wo.numberwidth = 2
-vim.o.colorcolumn = "80"
+vim.wo.number = true
+vim.wo.relativenumber = true
 
-vim.o.termguicolors = true
+vim.o.signcolumn = "yes"
+vim.o.numberwidth = 2
 
-vim.o.wrap = false
+vim.opt.colorcolumn = { 80 }
 
-vim.wo.cursorline = true
+vim.o.cursorline = true
 
 vim.o.showmode = false
 
--- autocommand to adjust formatoptions for all file types
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  command = "setlocal formatoptions-=cro",
-})
