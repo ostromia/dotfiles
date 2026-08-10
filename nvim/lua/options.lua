31ec52e96e795f6dffc574bb9b58f758395628a4
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2026-08-10 20:03:37 +0100

    [nvim] prevent nvim from deciding whether my indentation options are worth following or not

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -34,3 +34,13 @@ vim.api.nvim_create_autocmd("FileType", {
     callback = function() vim.opt_local.formatoptions:remove({ "c", "r", "o" }) end
 })
 
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    pattern = "*.typ",
+    callback = function()
+        vim.bo.tabstop = 4
+        vim.bo.softtabstop = 4
+        vim.bo.shiftwidth = 4
+        vim.bo.expandtab = true
+    end
+})
+
