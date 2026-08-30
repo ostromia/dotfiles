52575294b21e718d03b1568fb2051d75ed8bfe9a
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2026-08-30 18:03:14 +0100

    [pwsh] tweak path.ps1

--- powershell/path.ps1
+++ powershell/path.ps1
@@ -1,9 +1,8 @@
 function Sync-Path {
+	# rebuild current session's PATH
 	$env:Path = @(
 		$env:Path,
 		[Environment]::GetEnvironmentVariable('Path', 'User'),
 		[Environment]::GetEnvironmentVariable('Path', 'Machine')
-	) -split ";" | ? {$_} | % { $_.trimend("\") } | sort -unique | join-string ";"
+	) -split ";" | ? { $_ } | % { $_.trimend("\") } | sort -unique | join-string ";"
 }
-
-Sync-Path
