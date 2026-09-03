0c164c572bb279bbb041c1b4ec9d1fc20d0acf1e
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2026-09-03 17:27:49 +0100

    [pwsh] garbage

--- powershell/path.ps1
+++ powershell/path.ps1
@@ -1,8 +1,32 @@
+# function Sync-Path {
+#     [CmdletBinding()]
+#     param()
+
+#     $seen = [System.Collections.Generic.HashSet[string]]::new([System.StringComparer]::OrdinalIgnoreCase)
+#     $paths = [System.Collections.Generic.List[string]]::new()
+
+#     foreach ($source in @(
+#         $env:Path
+#         [Environment]::GetEnvironmentVariable('Path', 'Machine')
+#         [Environment]::GetEnvironmentVariable('Path', 'User')
+#     )) {
+#         foreach ($entry in ($source -split ';')) {
+#             $p = $entry.Trim().Trim('"').Trim()
+#             if (-not $p) { continue }
+#             if ($p -notmatch '^[A-Za-z]:\\$') { $p = $p.TrimEnd('\') }
+#             if ($seen.Add($p)) { $paths.Add($p) }
+#         }
+#     }
+
+#     $env:Path = $paths -join ';'
+# }
+
 function Sync-Path {
-	# rebuild current session's PATH
 	$env:Path = @(
 		$env:Path,
-		[Environment]::GetEnvironmentVariable('Path', 'User'),
-		[Environment]::GetEnvironmentVariable('Path', 'Machine')
+		[Environment]::GetEnvironmentVariable("Path", "User"),
+		[Environment]::GetEnvironmentVariable("Path", "Machine")
 	) -split ";" | ? { $_ } | % { $_.trimend("\") } | sort -unique | join-string ";"
 }
+
+
